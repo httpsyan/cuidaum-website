6f556be6068d00c045b6968e4ac1d6930fec40b4
Yan Castro <yancastrohenrique1@gmail.com>
2025-01-07 16:35:57 -0300

    feat: add Why Us section to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { Companies, Header, Hero } from "@/components";
+import { Companies, Header, Hero, WhyUs } from "@/components";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
-      <div className="mt-14">
+      <div className="my-14 flex flex-col space-y-14">
         <Companies />
+        <WhyUs />
       </div>
     </main>
   );
---
+++ src/components/why-us/why-us.tsx
@@ -0,0 +1,68 @@
+import { FaUsers } from "react-icons/fa";
+
+export const WhyUs = () => {
+  return (
+    <section className="container px-4 mx-auto">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h2 className="font-bold text-xl text-[#415EFF] mb-4">Por que nós?</h2>
+        <p className="font-bold text-3xl md:text-4xl text-[#0D2056] max-w-4xl mb-20">
+          Com soluções inovadoras e personalizadas, oferecemos total suporte e
+          cuidado para sua saúde.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16">
+          <article className="flex flex-col items-center space-y-6">
+            <div
+              className="bg-[#D5DDFC] rounded-[10px] p-6 flex items-center justify-center"
+              aria-hidden="true"
+            >
+              <FaUsers color="#1C44F2" size={24} />
+            </div>
+
+            <h3 className="text-black text-xl font-semibold">
+              Gestão completa
+            </h3>
+            <p className="text-[#72727B] text-base max-w-xs">
+              Monitore sua saúde com ferramentas que incluem planos
+              personalizados, alertas e relatórios detalhados.
+            </p>
+          </article>
+
+          <article className="flex flex-col items-center space-y-6">
+            <div
+              className="bg-[#D5DDFC] rounded-[10px] p-6 flex items-center justify-center"
+              aria-hidden="true"
+            >
+              <FaUsers color="#1C44F2" size={24} />
+            </div>
+
+            <h3 className="text-black text-xl font-semibold">
+              Comunicação Assertiva
+            </h3>
+            <p className="text-[#72727B] text-base max-w-xs">
+              Interação direta com profissionais de saúde para ajustes nas
+              rotinas e feedback contínuo.
+            </p>
+          </article>
+
+          <article className="flex flex-col items-center space-y-6">
+            <div
+              className="bg-[#D5DDFC] rounded-[10px] p-6 flex items-center justify-center"
+              aria-hidden="true"
+            >
+              <FaUsers color="#1C44F2" size={24} />
+            </div>
+
+            <h3 className="text-black text-xl font-semibold">
+              Equipe Especializada
+            </h3>
+            <p className="text-[#72727B] text-base max-w-xs">
+              Conte com médicos, fisioterapeutas, nutricionistas e outros
+              especialistas para um cuidado multidisciplinar.
+            </p>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
