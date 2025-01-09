9ae81a744c42abbd8ed496c6cea8c95ed1793b64
Yan Castro <yancastrohenrique1@gmail.com>
2025-01-09 19:19:52 -0300

    feat: add flaticon cdn domain to next image config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com", "cdn-icons-png.flaticon.com"],
   },
 };
 
